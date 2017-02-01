f77b0f153c53149f823ccbd284bd524e3cbe7495
alkisr <4alsahr@gmail.com>
2017-02-01 16:34:35 +0200

    Add files via upload

--- plugins/filter.lua
+++ plugins/filter.lua
@@ -1,19 +1,22 @@
 --[[
-#
-#ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#:((
-# For More Information ....! 
-# Developer : Aziz < @TH3_GHOST > 
-# our channel: @DevPointTeam
-# Version: 1.1
-#:))
-#ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#
-]]
+ _____ ____     ____   ___ _____ 
+|_   _|  _ \   | __ ) / _ \_   _|
+  | | | |_) |  |  _ \| | | || |  
+  | | |  __/   | |_) | |_| || |  
+  |_| |_|      |____/ \___/ |_|  
+                                 
+KASPER  TP (BY @kasper_dev)
+ _  __    _    ____  ____  _____ ____     _____ ____  
+| |/ /   / \  / ___||  _ \| ____|  _ \   |_   _|  _ \ 
+| ' /   / _ \ \___ \| |_) |  _| | |_) |    | | | |_) |
+| . \  / ___ \ ___) |  __/| |___|  _ <     | | |  __/ 
+|_|\_\/_/   \_\____/|_|   |_____|_| \_\    |_| |_|    
+                                                      
+--]]
 local function addword(msg, name)
     local hash = 'chat:'..msg.to.id..':badword'
-    redis:hset(hash, name, 'newword')
-    return "تم منع هذه الكلمة في المجموعة 📌📫\n>"..name
+    redis:hset(hash, name, 'nwrd')
+    return "تـ(✅)ـم حـضـ(📵)ـر الـكـ{🌝}ـلـمـة\n↜"..name
 end
 
 local function get_variables_hash(msg)
@@ -27,7 +30,7 @@ local function list_variablesbad(msg)
 
   if hash then
     local names = redis:hkeys(hash)
-    local text = 'قائمة الكلمات المحظورة 🚫🌀 :\n\n'
+    local text = '⇓ الـكـلـ(🔞)ـمـاتـ» الـمـحـ{🚨}ـضـوره ⇓\n\n'
     for i=1, #names do
       text = text..'> '..names[i]..'\n'
     end
@@ -41,7 +44,7 @@ function clear_commandbad(msg, var_name)
   --Save on redis  
   local hash = get_variables_hash(msg)
   redis:del(hash, var_name)
-  return 'تم مسح الكلمات الممنوعة 🏌⛳️'
+  return 'تـ{✅}ـم مـسـ(‼️)ـح الـكـلـ(🔞)ـمـاتـ» الـمـحـ{🚨}ـضـوره️'
 end
 
 local function list_variables2(msg, value)
@@ -79,27 +82,27 @@ function clear_commandsbad(msg, cmd_name)
   --Save on redis  
   local hash = get_variables_hash(msg)
   redis:hdel(hash, cmd_name)
-  return ''..cmd_name..'  تم السماح بالكلمة 🦁🎋'
+  return 'تـ(✅)ـم الـسـ{🚸}ـمـاح بـالـكـ(🌝)ـلـمـة\n↜'..cmd_name.. ' '
 end
 
 local function run(msg, matches)
-  if matches[2] == 'block' then
+  if matches[2] == 'f' or matches[2] == 'منع' then
   if not is_momod(msg) then
-   return 'للمدراء والادمنية فقط Ⓜ️💡'
+   return 'لـلـمـ{🏆}ـدراء فـقـ(‼️)ـط'
   end
   local name = string.sub(matches[3], 1, 50)
 
   local text = addword(msg, name)
   return text
   end
-  if matches[2] == 'blocks word' then
+  if matches[2] == 'a' or matches[2] == 'الكلمات المحضورة' then
   return list_variablesbad(msg)
-  elseif matches[2] == 'clean' then
-if not is_momod(msg) then return 'Only Owners :))' end
+  elseif matches[2] == 's' or matches[2] == 'مسح الكلمات' then
+if not is_momod(msg) then return 'لـلـمـ{🏆}ـدراء فـقـ(‼️)ـط' end
   local asd = '1'
     return clear_commandbad(msg, asd)
-  elseif matches[2] == 'unblock' or matches[2] == 'rw' then
-   if not is_momod(msg) then return 'Only Owners :))' end
+  elseif matches[2] == 'g' or matches[2] == 'الغاء منع' then
+   if not is_momod(msg) then return 'لـلـمـ{🏆}ـدراء فـقـ(‼️)ـط' end
     return clear_commandsbad(msg, matches[3])
   else
     local name = user_print_name(msg.from)
@@ -110,14 +113,30 @@ end
 
 return {
   patterns = {
-  "^([!/#])(rw) (.*)$",
-  "^([!/#])(block) (.*)$",
-   "^([!/#])(unblock) (.*)$",
-    "^([!/#])(blocks word)$",
-    "^([!/#])(clean) blocks word$",
+  "^([!/])(g) (.*)$",
+  "^([!/])(f) (.*)$",
+    "^([!/])(a)$",
+    "^([!/])(s)$",
+  "^()(الغاء منع) (.*)$",
+  "^()(منع) (.*)$",
+    "^()(الكلمات المحضورة)$",
+    "^()(مسح الكلمات)$",
 "^(.+)$",
-	   
   },
   run = run
 }
-
+--[[
+ _____ ____     ____   ___ _____ 
+|_   _|  _ \   | __ ) / _ \_   _|
+  | | | |_) |  |  _ \| | | || |  
+  | | |  __/   | |_) | |_| || |  
+  |_| |_|      |____/ \___/ |_|  
+                                 
+KASPER  TP (BY @kasper_dev)
+ _  __    _    ____  ____  _____ ____     _____ ____  
+| |/ /   / \  / ___||  _ \| ____|  _ \   |_   _|  _ \ 
+| ' /   / _ \ \___ \| |_) |  _| | |_) |    | | | |_) |
+| . \  / ___ \ ___) |  __/| |___|  _ <     | | |  __/ 
+|_|\_\/_/   \_\____/|_|   |_____|_| \_\    |_| |_|    
+                                                      
+--]]
